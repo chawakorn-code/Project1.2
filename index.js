1a52e71fdf452ba2a25b84902b801aec75ad12c8
athiccc <athi.lhong@gmail.com>
2020-03-20 00:18:53 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js')
+const papedBot = new Discord.Client()
+const moment = require('moment')
+
+const mongoose = require('mongoose')
+const { configDB } = require('./config/config')
+const employeeSchema = require('./model/employee')
+
+
+///////////////////
+///// Database/////
+///////////////////
+mongoose.connect(`mongodb://${configDB.dbHost}:${configDB.dbPort}/${configDB.dbName}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection
+  .once('open', () => console.log('Database is connected! === Discord CHECK-IN'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
+
+const Employee = mongoose.model('employee', employeeSchema)
+//////////////////
+///////////
+
+
+///////////////////
+///////// BOT /////
+///////////////////
+papedBot.login('NjkwMjM1ODYzOTQyMTY4Njk2.XnOehA.tRZbTV02NEB8yLCVu6bmrWILfwM', () => {
+}).catch((err) => console.log('err', err))
+
+papedBot.on('ready', () => {
+    console.log('Paped Ready!!!')
+})
+
+papedBot.on('message', async msg => {
+    const username = msg.author.username
+    const id = msg.author.discriminator
+    const text = msg.content
+    const currentChannel = msg.channel.id
+    const checkInChannel = '690221415928168469'
+
+    if(username !== 'ป้าเป็ด' && id !== '0060' && currentChannel === checkInChannel){
+        switch (text) {
+            case 'check':
+                await Employee.create({
+                    name: username,
+                    userId: id,
+                    startWork: Date.now()
+                })
+                msg.reply('บันทึกสำเร็จค่ะ มาทำงานเวลา : ' + moment().format('MMMM Do YYYY, h:mm:ss'))
+                break;
+            case 'bye':
+                await Employee.create({
+                    name: username,
+                    userId: id,
+                    endWork: Date.now()
+                })
+                msg.reply('บันทึกสำเร็จค่ะ ออกงานเวลา : ' + moment().format('MMMM Do YYYY, h:mm:ss'))
+                break;
+            default:
+                msg.reply('บันทึกผิดพลาดค่ะ โปรดลองใหม่อีกครั้ง')
+                break;
+        }
+
+    }
+
+});
+//////////////////
+/////////////
+/////////////////
